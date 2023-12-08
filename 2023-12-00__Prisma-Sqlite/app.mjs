230c5b8e42249e883159866903edd431e0bc6ec7
Georg Graf (SPG) <grafg@spengergasse.at>
2023-12-08 15:12:50 +0100

    autocommit

--- 2023-12-00__Prisma-Sqlite/app.mjs
+++ 2023-12-00__Prisma-Sqlite/app.mjs
@@ -1,6 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-//const { PrismaClient } = require("@prisma/client");
-
 const prisma = new PrismaClient({ log: ["query", "info"] });
 
 async function main() {
